2291de4b1308ed3a1a9dafe339c3f89d664585f9
alex <geniofuturo@gmail.com>
2020-08-10 16:08:26 -0300

    Add setup with main package 'xplugin_menu_favorite'.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xadmin-menu-favorite',
+    version='1.0.0',
+    packages=['xplugin_menu_favorite'],
+    url='https://github.com/alexsilva/xadmin-menu-favorite',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='Attach favorite menus to the list'
+)
