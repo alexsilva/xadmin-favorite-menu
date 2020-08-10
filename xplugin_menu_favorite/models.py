e59023a1c8c7f1bf16dba0847495f4cc300996d7
alex <geniofuturo@gmail.com>
2020-08-10 16:23:32 -0300

    Add MenuFavorite.

---
+++ xplugin_menu_favorite/models.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.utils.translation import ungettext_lazy as _
+
+
+class MenuFavorite(models.Model):
+    """Stores data about the created menus"""
+    order = models.PositiveIntegerField(verbose_name=_("Order"))
+    content_type = models.ForeignKey(ContentType, verbose_name=_("Content"))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("User"))
+
+    class Meta:
+        verbose_name = _("Favorite Menu (plugin)")
+
+    def __str__(self):
+        return f"{self.user}/{self.order}"
