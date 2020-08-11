91fe7161a9a94098e30abf093d6ad5b488e4e56c
alex <geniofuturo@gmail.com>
2020-08-11 12:28:48 -0300

    Rename 'menu_favorite_template_using' or 'menu_favorite_render_using'

--- xplugin_menu_favorite/plugin.py
+++ xplugin_menu_favorite/plugin.py
@@ -13,7 +13,7 @@ class MenuFavoritePlugin(BaseAdminPlugin):
     Plugin that allows you to add favorite menus to the site menu
     """
     menu_favorite_template = "xadmin/menu_favorite/menus.html"
-    menu_favorite_template_using = None  # template engine (def. django)
+    menu_favorite_render_using = None  # template engine (def. django)
     menu_favorite = True
 
     def init_request(self, *args, **kwargs):
@@ -30,7 +30,7 @@ class MenuFavoritePlugin(BaseAdminPlugin):
         }
         content = render_to_string("xadmin/menu_favorite/menus_btn_top_toolbar.html",
                                    context=get_context_dict(context),
-                                   using=self.menu_favorite_template_using)
+                                   using=self.menu_favorite_render_using)
         nodes.insert(0, content)
 
     def block_extra_slide_menu(self, context, nodes):
@@ -40,7 +40,7 @@ class MenuFavoritePlugin(BaseAdminPlugin):
             'menus': MenuFavorite.objects.all()
         }
         nodes.append(render_to_string(self.menu_favorite_template,
-                                      using=self.menu_favorite_template_using,
+                                      using=self.menu_favorite_render_using,
                                       context=get_context_dict(context)))
 
     def block_extrabody(self, context, nodes):
