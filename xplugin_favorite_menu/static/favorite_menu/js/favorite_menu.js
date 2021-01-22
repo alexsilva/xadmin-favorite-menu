fc5708884ee505bc5282a82afd0582a87e14a945
alex <alex@fabricadigital.com.br>
2021-01-22 18:30:06 -0300

    Organizing the code.

--- xplugin_favorite_menu/static/favorite_menu/js/favorite_menu.js
+++ xplugin_favorite_menu/static/favorite_menu/js/favorite_menu.js
@@ -1,54 +1,54 @@
 $(function () {
-    $.fn.favorite_menu = function (options) {
-        var MenuFavorite = function ($this, options) {
-            this.$this = $this;
-            this.options = options;
-        }
+    var MenuFavorite = function ($this, options) {
+        this.$this = $this;
+        this.options = options;
+    }
 
-        MenuFavorite.prototype.before_send_post = function (xhr, settings) {
-            xhr.setRequestHeader("X-CSRFToken", $.getCookie('csrftoken'));
-        }
+    MenuFavorite.prototype.before_send_post = function (xhr, settings) {
+        xhr.setRequestHeader("X-CSRFToken", $.getCookie('csrftoken'));
+    }
 
-        MenuFavorite.prototype.post_done = function (data, text, xhr) {
-            if (data.status === true) {
-                location.reload(true);
-            } else {
-                alert(gettext("failed to create/delete menu."));
-            }
-        }
-
-        MenuFavorite.prototype.post_fail = function (xhr) {
+    MenuFavorite.prototype.post_done = function (data, text, xhr) {
+        if (data.status === true) {
+            location.reload(true);
+        } else {
             alert(gettext("failed to create/delete menu."));
         }
+    }
 
-        MenuFavorite.prototype.request = function () {
-            var self = this;
-            this.$this.addClass("disabled");
-            return $.ajax({
-                type: "POST",
-                url: this.$this.data('ajax-url'),
-                data: this.options.data || {},
-                dataType: "json",
-                beforeSend: this.before_send_post,
-            }).done(
-                $.proxy(this.post_done, this)
-            ).fail(
-                $.proxy(this.post_fail, this)
-            ).always(function() {
-                self.$this.removeClass("disabled");
-            });
-        }
+    MenuFavorite.prototype.post_fail = function (xhr) {
+        alert(gettext("failed to create/delete menu."));
+    }
 
-        // get option value by name
-        MenuFavorite.prototype.get_option = function (name){
-            return this.options[name];
-        }
+    MenuFavorite.prototype.request = function () {
+        var self = this;
+        this.$this.addClass("disabled");
+        return $.ajax({
+            type: "POST",
+            url: this.$this.data('ajax-url'),
+            data: this.options.data || {},
+            dataType: "json",
+            beforeSend: this.before_send_post,
+        }).done(
+            $.proxy(this.post_done, this)
+        ).fail(
+            $.proxy(this.post_fail, this)
+        ).always(function () {
+            self.$this.removeClass("disabled");
+        });
+    }
 
-        //action bind
-        MenuFavorite.prototype.bind_click = function () {
-            this.$this.click($.proxy(this.request, this));
-        }
+    // get option value by name
+    MenuFavorite.prototype.get_option = function (name) {
+        return this.options[name];
+    }
 
+    //action bind
+    MenuFavorite.prototype.bind_click = function () {
+        this.$this.click($.proxy(this.request, this));
+    }
+
+    $.fn.favorite_menu = function (options) {
         if (!this.data('favorite_menu')) {
             this.data('favorite_menu', new MenuFavorite(this, options));
         }
