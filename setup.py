ae2c7714cb2ce3fd8d1eea6835ad3e3fef8fc750
alexsilva <geniofuturo@gmail.com>
2022-11-10 16:01:21 -0300

    v1.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='xadmin-favorite-menu',
-    version='1.3.1',
+    version='1.4.0',
     packages=['xplugin_favorite_menu'],
     url='https://github.com/alexsilva/xadmin-favorite-menu',
     license='MIT',
