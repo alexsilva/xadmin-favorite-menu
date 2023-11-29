725ba148b42502309acdb1804b9e16eb11e83b27
alexsilva <geniofuturo@gmail.com>
2023-11-29 15:13:37 -0300

    Replacing 'ugettext_lazy' to 'gettext_lazy'.

--- xplugin_favorite_menu/models.py
+++ xplugin_favorite_menu/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class FavoriteMenuManager(models.Manager):
